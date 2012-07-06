3cdcec667f63e4295218b85ea72a0f0c861da6bd
juffel <julian.dobmann@googlemail.com>
2012-07-06 17:35:04 +0200

    irgendeinen initkrams

---
+++ src/dbsProjekt/ImportData.java
@@ -0,0 +1,9 @@
+package dbsProjekt;
+
+/**
+ * @author julian
+ *
+ */
+public class ImportData {
+
+}
---
+++ src/dbsProjekt/Main.java
@@ -0,0 +1,11 @@
+package dbsProjekt;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		
+		
+	}
+
+}
