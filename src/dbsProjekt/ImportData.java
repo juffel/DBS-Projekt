ca8251ad9c83df45b3c1f2ef4f63f3bb4d823219
juffel <julian.dobmann@googlemail.com>
2012-07-06 17:50:34 +0200

    openConnection(...) implementiert, nicht getestet

--- src/dbsProjekt/ImportData.java
+++ src/dbsProjekt/ImportData.java
@@ -1,9 +1,57 @@
 package dbsProjekt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
+ * Mithilfe dieser Klasse kann man eine Datenbank erzeugen, die unserem Schema entspricht.
  * @author julian
  *
  */
 public class ImportData {
+	
+	/**
+	 * baut Verbindung zur angegebenen Datenbank auf
+	 */
+	private void openConnection(String server, String port, String name, String user, String password) {
+		
+		try{
+			// Treiber laden
+			Class.forName("org.postgresql.Driver");
+		}
+		catch(ClassNotFoundException cnfe){
+			
+			// Treiber konnte nicht geladen werden!
+			System.out.println("PostgreSQL Treiber konnte nicht gefunden werden.");
+			
+			// Stacktrace ausgeben um mögliche Fehler nachzuvollziehen
+			cnfe.printStackTrace();
+			System.exit(0);
+		}
+		
+		// Treiber konnte geladen werden!
+		System.out.println("Der Treiber wurde erfolgreich registriert!");
+		
+		// Kann eine Verbindung zur Datenbank etabliert werden?
+		Connection conn = null;
+		
+		try{
+			// Verbindung zur Datenbank herstellen
+			conn = DriverManager.getConnection("jdbc:postgresql://" + server + ":" + port + "/" + name, user , password);
+		}
+		catch (SQLException sqle){
+			// Verbindung konnte nicht hergestellt werden!
+			System.out.println("Verbindungkonnte nicht hergestellt werden!");
+			
+			// Stacktrace ausgeben um mögliche Fehler nachzuvollziehen
+			sqle.printStackTrace();
+			System.exit(0);
+		}
+		
+		// Verbindung konnte hergestellt werden!
+		System.out.println("Die Verbingung zur Datenbank wurde hergestellt.");
+	}
+	
 
 }
