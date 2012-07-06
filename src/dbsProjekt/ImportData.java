f65718e08e82a9476b91dd1489d4b7fb726a708e
juffel <julian.dobmann@googlemail.com>
2012-07-07 01:35:59 +0200

    importData.init() implementiert

--- src/dbsProjekt/ImportData.java
+++ src/dbsProjekt/ImportData.java
@@ -1,8 +1,11 @@
 package dbsProjekt;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Mithilfe dieser Klasse kann man eine Datenbank erzeugen, die unserem Schema entspricht.
@@ -16,6 +19,51 @@ public class ImportData {
 	 * @param file
 	 */
 	public void init(String file) {
+		
+		File input = new File("./lange_nacht_postgres.sql");
+		BufferedReader rdr;
+		
+		try {
+			
+			rdr = new BufferedReader(new FileReader(input));
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			return;
+		}
+		
+		LinkedList<String> queries = new LinkedList<String>();
+		
+		try {
+			
+			// die Datei Zeichen für Zeichen auslesen
+			int c;
+			StringBuffer buf = new StringBuffer();
+			
+			// und die einzelnen Queries zusammenfassen, also immer bis zu einem Semikolons
+			while((c = rdr.read()) != -1) {
+
+				if(c == 59) { // ascii 59 = ';'
+					
+					queries.add(buf.toString()); // Query in queries abheften
+					buf.delete(0, buf.length()); // und Buffer reseten
+					
+				} else {
+					
+					buf.append((char) c);
+					
+				}
+				
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Queries ausführen
+		
+		System.out.println("breakpoint");
+
 		
 	}
 	
